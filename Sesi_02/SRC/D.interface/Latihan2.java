93182ff3966c6116672d24f48e657db5fa1e2609
Sukiyatno_nugroho <nugrohosukiyatno@yahoo.com>
2011-10-19 14:52:48 +0700

    tugas

---
+++ Sesi_02/SRC/D.interface/Latihan2.java
@@ -0,0 +1,7 @@
+Public class Latihan1{
+ Latihan 1=new Latihan();
+ public static void main (String[]args[]){
+Latihan l2=new Latihan("andi");
+Latihan l3=new Latihan("indra",1);
+ }
+}
---
+++ Sesi_03/Belajar4.java
@@ -0,0 +1,23 @@
+public class Belajar4{
+	private String nama ;
+	public static void main(String[] args){
+		Belajar test ;
+		test = new Belajar4();
+		test.setNama ("andi");
+		byte b = 100;
+		short s = 100;
+		
+		Belajar4 belajarvariabel = test;
+		belajarVariabel.setNama ("belajar variabel");
+		
+		System.out.println(b + s);
+		System.out.println("nama : " + test.getNama() + " " + b);
+		}
+		public void setNama (String aNama){
+			this.nama=aNama;
+			}
+		public String getNama(){
+			return this.nama;
+		}
+	}
+		
